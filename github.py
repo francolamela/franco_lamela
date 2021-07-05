5cad2a957a7e66117af91e5ba5c4c20a10797868
francolamela <86728171+francolamela@users.noreply.github.com>
2021-07-05 15:06:11 -0300

    Create github.py

---
+++ github.py
@@ -0,0 +1,12 @@
+print("hello world")
+
+num = 2
+
+if num > 1:
+  print ("Correcto")
+ else:
+  print("incorrecto")
+  
+  
+api_key = dskljdasj4lj4rj35j3oi5u394
+secret = lk5jl34kj5kl34j5kl345jk43lj5k34lj5kl34j5k3l
